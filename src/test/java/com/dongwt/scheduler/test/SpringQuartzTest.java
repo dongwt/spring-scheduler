68b181aa80ac49803ac28435e1e1fbdde6926386
董 纹陶 [SVR III] <dongwentao@lifang.com>
2017-04-26 21:27:35 +0800

    quartz

--- src/test/java/com/dongwt/scheduler/test/SpringQuartzTest.java
+++ src/test/java/com/dongwt/scheduler/test/SpringQuartzTest.java
@@ -1,7 +1,6 @@
 package com.dongwt.scheduler.test;
 
 import com.dongwt.scheduler.job.FailJob;
-import com.dongwt.scheduler.utils.QuartzUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.quartz.*;
@@ -14,26 +13,15 @@ import java.util.concurrent.TimeUnit;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = {"classpath*:spring-quartz.xml"})
 public class SpringQuartzTest {
-    
-    @Test
-    public void baseTest() {
-        try {
-            QuartzUtils.baseTest();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @Test
-    public void testFailJob() throws SchedulerException, InterruptedException {
-        SchedulerFactory sf = new StdSchedulerFactory();
-        Scheduler sc = sf.getScheduler();
-        JobDetail job1 = JobBuilder.newJob(FailJob.class).withIdentity("job1", "group1").build();
-        Trigger trigger1 = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startNow().build();
-        sc.scheduleJob(job1, trigger1);
-        sc.start();
-        TimeUnit.DAYS.sleep(1);
+    public void test(){
+        try {
+            Thread.sleep(1000*60);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
